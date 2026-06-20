0dc1f73c8481ed05702bf3c79c995195b77fff4a
Manoj Naidu <manojnaidu619@gmail.com>
2026-06-20 17:38:30 +0530

    Add NEXT_PUBLIC_STRIPE_LIFETIME_PLAN_ID to environment configuration

--- env.mjs
+++ env.mjs
@@ -22,6 +22,7 @@ export const env = createEnv({
     NEXT_PUBLIC_STRIPE_PRO_YEARLY_PLAN_ID: z.string().min(1),
     NEXT_PUBLIC_STRIPE_BUSINESS_MONTHLY_PLAN_ID: z.string().min(1),
     NEXT_PUBLIC_STRIPE_BUSINESS_YEARLY_PLAN_ID: z.string().min(1),
+    NEXT_PUBLIC_STRIPE_LIFETIME_PLAN_ID: z.string().min(1),
   },
   runtimeEnv: {
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
@@ -44,5 +45,7 @@ export const env = createEnv({
       process.env.NEXT_PUBLIC_STRIPE_BUSINESS_MONTHLY_PLAN_ID,
     NEXT_PUBLIC_STRIPE_BUSINESS_YEARLY_PLAN_ID:
       process.env.NEXT_PUBLIC_STRIPE_BUSINESS_YEARLY_PLAN_ID,
+    NEXT_PUBLIC_STRIPE_LIFETIME_PLAN_ID:
+      process.env.NEXT_PUBLIC_STRIPE_LIFETIME_PLAN_ID,
   },
 });
